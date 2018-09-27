e416c5c022d25814a99219c9aea68adb68be65ba
Sean-HHH <Sean-HHH@users.noreply.github.com>
2018-09-27 10:37:31 +0000

    20180927T103702Z

---
+++ mario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Input Height:");
+    }
+    while(n<= 0 || n >= 24);
+
+    for (int i=1; i<=n; i++)
+    {
+        for (int j=n-i; j>0;j--)
+        {
+            printf(" ");
+        }
+        for (int k=0; k<i; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int l=0; l<i; l++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
