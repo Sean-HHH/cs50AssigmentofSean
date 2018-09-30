240d89c37bdb2e59cf2862fd2c926770a2741b67
Sean-HHH <Sean-HHH@users.noreply.github.com>
2018-09-30 07:03:09 +0000

    20180930T070113Z

--- cash.c
+++ cash.c
@@ -4,45 +4,50 @@
 
 int main(void)
 {
+    //declaim variables for coins and user inputs
     float owed;
-    int quarter=0;
-    int dime=0;
-    int nickel=0;
-    int cent=0;
+    int quarter = 0;
+    int dime = 0;
+    int nickel = 0;
+    int cent = 0;
 
+    //ask for user inputs and it must be valid according to the spec
     do
     {
         owed = get_float("Amounts Owed:");
     }
-    while(owed<=0);
+    while (owed <= 0);
 
+    //convert and round the float
     owed = owed * 100;
     owed = round(owed);
 
-    while(owed>=25)
+    //to decide what kinds of coins to giveout
+    while (owed >= 25)
     {
         quarter++;
-        owed = owed-25;
+        owed = owed - 25;
     }
 
-    while(owed>=10)
+    while (owed >= 10)
     {
         dime++;
-        owed = owed-10;
+        owed = owed - 10;
     }
 
-    while(owed>=5)
+    while (owed >= 5)
     {
         nickel++;
-        owed = owed-5;
+        owed = owed - 5;
     }
 
-    while(owed>=1)
+    while (owed >= 1)
     {
         cent++;
-        owed = owed-1;
+        owed = owed - 1;
     }
-    int coins = quarter+dime+nickel+cent;
+    int coins = quarter + dime + nickel + cent;
 
-    printf("%i\n",coins);
+    //show the final results
+    printf("%i\n", coins);
 }
