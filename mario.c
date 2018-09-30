1d05c6a7bc8d01aa1dfc6d97978b97e02222fbf5
Sean-HHH <Sean-HHH@users.noreply.github.com>
2018-09-30 06:51:11 +0000

    20180930T065033Z

--- mario.c
+++ mario.c
@@ -3,24 +3,31 @@
 
 int main(void)
 {
+    //get user's inputs and it has to be a valid number according to the spec
     int n;
     do
     {
         n = get_int("Input Height:");
     }
-    while(n<0 || n >= 24);
+    while(n<0 || n>=24);
 
+    //Loop to print the pyrimid
     for (int i=1; i<=n; i++)
     {
+        //print space before the left pyrimid
         for (int j=n-i; j>0;j--)
         {
             printf(" ");
         }
+        //print # for the left pyrimid
         for (int k=0; k<i; k++)
         {
             printf("#");
         }
+        //print space in the middle
         printf("  ");
+
+        //print # in the right
         for (int l=0; l<i; l++)
         {
             printf("#");
