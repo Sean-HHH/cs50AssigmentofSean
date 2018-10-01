93875aef6ca17d1b4b820fd040fb20f16c317c56
Sean-HHH <Sean-HHH@users.noreply.github.com>
2018-10-01 08:39:40 +0000

    20181001T083911Z

--- vigenere.c
+++ vigenere.c
@@ -28,7 +28,7 @@ int main(int argc, string argv[])
     int pl = strlen(p);
 
 //Print ciphered char
-    printf("cipheretext:";)
+    printf("cipheretext:");
     for (int i = 0, j = 0; i < pl; i++ )
     {
         //print alphabet, upper, lower case, others than alphabet repectively
